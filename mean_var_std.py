f2f6946f3873c5be96972a30677a76756906d2f4
nitipat21 <101240773+nitipat21@users.noreply.github.com>
2022-07-26 22:36:41 +1000

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,14 @@ import numpy as np
 
 def calculate(list):
 
-
+    # {
+    #   'mean': [axis1, axis2, flattened],
+    #   'variance': [axis1, axis2, flattened],
+    #   'standard deviation': [axis1, axis2, flattened],
+    #   'max': [axis1, axis2, flattened],
+    #   'min': [axis1, axis2, flattened],
+    #   'sum': [axis1, axis2, flattened]
+    # }
 
 
     return calculations
